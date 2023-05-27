3ac5e019f9224b673b2bb8973f71b699673c3eb1
DeadFishEyes2 <122477630+DeadFishEyes2@users.noreply.github.com>
2023-05-27 09:50:31 +0300

    Toggle Audio Button color

--- settings.js
+++ settings.js
@@ -20,15 +20,19 @@ cheatButton.addEventListener('click', () => {
 //toggle audio
 const toggleAudio = document.getElementById("toggleAudio");
 
+toggleAudio.style.backgroundColor = "green";
+
 toggleAudio.addEventListener('click', () => {
     if (audio.volume == 0){
         audio.volume = 1;
         audioPop.volume = 1;
         audioError.volume = 1;
+        toggleAudio.style.backgroundColor = "green";
     } else {
         audio.volume = 0;
         audioPop.volume = 0;
         audioError.volume = 0;
+        toggleAudio.style.backgroundColor = "crimson";
     }
 })
 
